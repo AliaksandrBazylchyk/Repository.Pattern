409b36eb265a81679b44196ce5dc3aeabf903464
Aliaksandr Bazylchyk <bazylchyk.a@outlook.com>
2022-04-13 15:38:34 +0300

    Repository implementation with example class

--- Repository.Pattern/Program.cs
+++ Repository.Pattern/Program.cs
@@ -1,5 +1,11 @@
 using Repository.Pattern.Common.Options;
 
+// Data Access Layer
+using Repository.Pattern.DAL.Base;
+using Repository.Pattern.DAL.Base.Implementation;
+using Repository.Pattern.DAL.Contexts;
+using Repository.Pattern.DAL.Repositories;
+using Repository.Pattern.DAL.Repositories.Implementation;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -7,10 +13,15 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Options loader
+builder.Services.AddDbContext<PostgresDbContext>();
+
+// Options loader section
 builder.Services.Configure<ConnectionStrings>(
     builder.Configuration.GetSection(ConnectionStrings.Section));
 
+// Repositories loader section
+builder.Services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
+builder.Services.AddScoped(typeof(IExampleRepository), typeof(ExampleRepository));
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
